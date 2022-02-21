57a9e237642786f94bd6fbbfb377e338107f9d41
Beep <beepnboopn@gmail.com>
2022-02-20 19:29:06 -0500

    Update battle-animations-moves.ts

--- src/battle-animations-moves.ts
+++ src/battle-animations-moves.ts
@@ -18051,51 +18051,6 @@ const BattleMoveAnims: AnimTable = {
 			}, 'ballistic');
 		},
 	},
-	permafrost: {
-		anim(scene, [attacker, defender]) {
-			scene.showEffect('icicle', {
-				x: attacker.x,
-				y: attacker.y,
-				z: attacker.z,
-				scale: 0.1,
-				opacity: 0.5,
-			}, {
-				x: -25,
-				y: defender.y - 40,
-				z: defender.z,
-				scale: 0.3,
-				opacity: 1,
-			}, 'ballistic');
-			scene.showEffect('icicle', {
-				x: attacker.x,
-				y: attacker.y,
-				z: attacker.z,
-				scale: 0.1,
-				opacity: 0.5,
-				time: 125,
-			}, {
-				x: +50,
-				y: defender.y - 40,
-				z: defender.z,
-				scale: 0.3,
-				opacity: 1,
-			}, 'ballistic');
-			scene.showEffect('icicle', {
-				x: attacker.x,
-				y: attacker.y,
-				z: attacker.z,
-				scale: 0.1,
-				opacity: 0.5,
-				time: 250,
-			}, {
-				x: +30,
-				y: defender.y - 45,
-				z: defender.z,
-				scale: 0.3,
-				opacity: 1,
-			}, 'ballistic');
-		},
-	},
 	toxicspikes: {
 		anim(scene, [attacker, defender]) {
 			scene.showEffect('poisoncaltrop', {
